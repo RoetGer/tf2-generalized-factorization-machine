3d3ec25e39c4d0de1e1afa7197b58416e5ae7868
Gernot Roetzer <gernot.roetzer@gmail.com>
2021-03-03 23:56:44 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="tf2_gfm",
+    version="0.0.1",
+    url="https://github.com/RoetGer/tf2-generalized-factorization-machine",
+    packages=find_packages(include=["tf2_gfm"])
+)
