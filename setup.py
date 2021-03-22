d23b017018d7c8fb06e1143d4c3932115b894b10
Gernot Roetzer <gernot.roetzer@gmail.com>
2021-03-22 21:24:23 +0100

    Add longdescription to setup.py.

--- setup.py
+++ setup.py
@@ -1,8 +1,17 @@
+import os.path as osp
 from setuptools import setup, find_packages
+
+
+# read the contents of your README file
+rep_folder = osp.abspath(osp.dirname(__file__))
+with open(osp.join(rep_folder, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
 
 setup(
     name="tf2_gfm",
     version="0.0.1",
     url="https://github.com/RoetGer/tf2-generalized-factorization-machine",
-    packages=find_packages(include=["tf2_gfm"])
+    packages=find_packages(include=["tf2_gfm"]),
+    long_description=long_description, 
+    long_description_content_type="text/markdown"
 )
